cdc3621cdceb2debd21cc62cea086e7f79315ae4
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-03 18:43:43 +0100

    Añado rombo hueco

---
+++ src/rombohueco.java
@@ -0,0 +1,32 @@
+public class rombohueco{
+    public static void main(String[] args) {
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura del rombo: "));
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = altura; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i * 2; j++) {
+                if (j == 0 || j == 2 * i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < altura; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * (altura - i)) - 1; j++) {
+                if (j == 0 || j == ((2 * (altura - i)) - 1) - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
